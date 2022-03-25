15d18eb84f6458f9b37e4b3d50b3af4415028c26
Arran France <arran@arranfrance.com>
2022-03-25 14:44:02 +0000

    fix: poor formatting of the .npmrc file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
-echo "registry = https://npm.pkg.github.com/$INPUT_ORG_NAME" > .npmrc
+if [[ -z "$INPUT_ORG_NAME" ]]; then
+    echo "@tendable:registry=https://npm.pkg.github.com/" > .npmrc
+else
+    echo "@$INPUT_ORG_NAME:registry=https://npm.pkg.github.com/" > .npmrc
+fi
 if [[ -z "$INPUT_AUTH_TOKEN" ]]; then
     echo "//npm.pkg.github.com/:_authToken=$AUTH_TOKEN" >> .npmrc
 else
